91276eeb9e9ccea051c6f6c0e483a973c93c3221
Mohammed Muthmayeen N <86474952+MuthmayeenN02@users.noreply.github.com>
2023-01-08 21:02:18 +0530

    Update reverseNumber.java

--- reverseNumber.java
+++ reverseNumber.java
@@ -5,16 +5,16 @@ public class Main
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		Scanner input = new Scanner(System.in);
-        int num = input.nextInt();
-        int number = num;
-        int rev = 0;
-        int last = 0;
-        do{
-            last = num % 10;
-            rev = rev*10 + last;
-            num = num / 10;
-        }while(num != 0);
-        System.out.println("reverse of " + number + " is " + rev);
+        	int num = input.nextInt();
+        	int number = num;
+       		int rev = 0;
+        	int last = 0;
+       		do{
+		    last = num % 10;
+		    rev = rev*10 + last;
+		    num = num / 10;
+		}while(num != 0);
+		System.out.println("reverse of " + number + " is " + rev);
 	}
 }
 
