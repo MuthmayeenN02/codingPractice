652c7f052c24872289bdf134da70b63f3b1fb4d4
Mohammed Muthmayeen N <86474952+MuthmayeenN02@users.noreply.github.com>
2023-01-09 21:26:04 +0530

    Update countVowels.java

--- countVowels.java
+++ countVowels.java
@@ -1,6 +1,6 @@
 // calculating count of vowels and consonents
-import java.util.Scanner;
-class Main {
+import java.util.*;
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         Scanner input = new Scanner(System.in);
