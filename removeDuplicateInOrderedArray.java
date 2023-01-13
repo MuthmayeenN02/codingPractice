aebbb110c700626cddc269dd7a268116e24dfe1d
Mohammed Muthmayeen N <86474952+MuthmayeenN02@users.noreply.github.com>
2023-01-13 21:29:47 +0530

    Create removeDuplicateInOrderedArray.java

---
+++ removeDuplicateInOrderedArray.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static int removeduplicates(int a[], int n)
+    {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int j = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (a[i] != a[i + 1]) {
+                a[j++] = a[i];
+            }
+        }
+ 
+        a[j++] = a[n - 1];
+ 
+        return j;
+    }
+    public static void main(String[] args){
+        int a[] = { 1, 1, 2, 2, 2,3 };
+        int n = a.length;
+        n = removeduplicates(a, n);
+        // Printing The array elements
+        for (int i = 0; i < n; i++)
+            System.out.print(a[i] + " ");
+    }
+}
