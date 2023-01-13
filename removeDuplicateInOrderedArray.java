eea0a58b78215f50b6a2e05ec6d40480a6614d36
Mohammed Muthmayeen N <86474952+MuthmayeenN02@users.noreply.github.com>
2023-01-13 21:34:44 +0530

    Update removeDuplicateInOrderedArray.java

--- removeDuplicateInOrderedArray.java
+++ removeDuplicateInOrderedArray.java
@@ -5,13 +5,13 @@ public class Main {
             return n;
         }
         int j = 0;
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n - 1; i++) { // comparing two elements if first two is not same put it
             if (a[i] != a[i + 1]) {
                 a[j++] = a[i];
             }
         }
  
-        a[j++] = a[n - 1];
+        a[j++] = a[n - 1]; // adding last element
  
         return j;
     }
