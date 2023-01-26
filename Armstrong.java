cdce8b6674f3a267b4140e09051101fba54eaa6f
Mohammed Muthmayeen N <86474952+MuthmayeenN02@users.noreply.github.com>
2023-01-26 19:10:32 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -36,3 +36,30 @@ public class Main
 // true
 // false
 // true
+
+// Python
+def order(x):
+    n = 0
+    while (x != 0):
+        n = n+1
+        x = x/10
+    return n
+
+def isArmstrong(x):
+    n = order(x)
+    temp = x
+    sum1 = 0
+    while (temp != 0):
+        r = temp % 10
+        sum1 = sum1 + pow(r, n)
+        temp = temp/10
+ 
+    # If condition satisfies
+    return (sum1 == x)
+ 
+ 
+# Driver Program
+x = 153
+print(isArmstrong(x))
+x = 1253
+print(isArmstrong(x))
